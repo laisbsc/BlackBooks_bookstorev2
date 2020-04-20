aa1af1c7706cf745305153094662c6b64f8340ed
laisbsc <santanal@tcd.ie>
2020-04-20 08:13:43 +0100

    implementation of CRUD functions

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,6 +3,6 @@ module.exports = {
     url: 'mongodb://localhost:27017'
 
     //MongoDB Atlas link
-    //url: 'mongodb+srv://test:<pwwd>@cluster0-izwzo.mongodb.net/test?retryWrites=true&w=majority'
+    //url: 'mongodb+srv://test:fv4fKTlI2O9236Xi@cluster0-izwzo.mongodb.net/test?retryWrites=true&w=majority'
     //REMEMBER TO MAKE THIS A VARIABLE! > hide password from there
   };
