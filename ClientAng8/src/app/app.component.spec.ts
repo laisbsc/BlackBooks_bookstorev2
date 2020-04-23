2bc8fbe6c5c92a5008306d04c40fff7fc06bd495
laisbsc <santanal@tcd.ie>
2020-04-23 07:48:31 +0100

    fixing setup - dbugging local deployment

--- ClientAng8/src/app/app.component.spec.ts
+++ ClientAng8/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mean-course'`, () => {
+  it(`should have as title 'ClientAng8'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mean-course');
+    expect(app.title).toEqual('ClientAng8');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mean-course app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ClientAng8 app is running!');
   });
 });
