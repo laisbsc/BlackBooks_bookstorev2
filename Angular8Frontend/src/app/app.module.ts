d29373d6fed33a41197122d81cb49fe06928ba6f
laisbsc <santanal@tcd.ie>
2020-04-23 07:26:05 +0100

    setting up components and services structure

--- Angular8Frontend/src/app/app.module.ts
+++ Angular8Frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import {MatNativeDateModule} from '@angular/material/core';
+import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
@@ -11,7 +11,7 @@ import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookCreateComponent } from './books/book-create/book-create.compose';
+import { BookCreateComponent } from './books/components/book-create/book-create.compose';
 
 
 @NgModule({
